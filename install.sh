049e1dc7c97dbca34767297cf32e94b8abbbf679
vsaltel <saltel.valentin@gmail.com>
2022-12-08 16:16:43 +0100

    Installation working without press ENTER needed

--- install.sh
+++ install.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-DIRPATH=$(dirname ${0})
-LOGFILE="${DIRPATH}/logs"
+## COLORS   ##
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+RED='\033[0;32m'
+YELLOW='\033[1;33m'
 
+## VARS     ## 
+DIRPATH=$(dirname ${0})
+LOGFILE="${DIRPATH}/logs.txt"
+
+## BEGIN    ##
 if [ "$EUID" -eq 0 ]; then
     if [ -n "${SUDO_USER}" ]; then
         USERNAME=${SUDO_USER}
     elif [ -n "${1}" ]; then
         USERNAME=${1}
     else
-        echo -e "Usage :\n\tsudo ${0}\n\tor\n\t${0} [USER]"
+        echo -e "${RED}Usage :\n\tsudo ${0}\n\tor\n\t${0} [USER]${NC}"
         exit
     fi
 
     if [ -n ${USERNAME} ]; then
         USERHOME=$(getent passwd ${USERNAME} | cut -d: -f6)
     else
-        echo "User not find"
+        echo -e "${RED}User not find${NC}"
         exit
     fi
 
     if [ -z ${USERHOME} ]; then
-        echo "${USERNAME} not exist on the system"
+        echo -e "${RED}${USERNAME} not exist on the system${NC}"
         exit
     fi
 else
-    echo "Please run as root"
+    echo -e "${RED}Please run as root${NC}"
     exit
 fi
 
@@ -58,8 +66,14 @@ echo "COPY CONFIG FILES"
 cp $DIRPATH/srcs/tmux.conf $USERHOME/.tmux.conf
 cp $DIRPATH/srcs/vimrc $USERHOME/.vimrc
 
-echo "INSTALL VIM PLUGINS"
-su ${USERNAME} -c "vim +PlugInstall +quitall" >> $LOGFILE
+echo "INSTALL VIM PLUGIN MANAGER"
+su ${USERNAME} -c "curl -fLo $USERHOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" &>> $LOGFILE
+
+echo -e "${YELLOW}INSTALL VIM PLUGINS${NC}"
+su ${USERNAME} -c "vim +'PlugInstall --sync' +quitall" &>> $LOGFILE
 
 echo "INSTALL YOUCOMPLETEME PLUGIN"
 su ${USERNAME} -c "python3 $USERHOME/.vim/plugged/YouCompleteMe/install.py" >> $LOGFILE
+
+echo -e "${GREEN}INSTALLATION SUCCESSFULL !${NC}"

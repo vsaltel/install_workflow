b4e56f0182472c66b065d81435f9345087ae9ea5
vsaltel <saltel.valentin@gmail.com>
2022-12-08 17:07:15 +0100

    tee logs + header

--- install.sh
+++ install.sh
@@ -19,57 +19,57 @@ if [ "$EUID" -eq 0 ]; then
     elif [ -n "${1}" ]; then
         USERNAME=${1}
     else
-        echo -e "${RED}Usage :\n\tsudo ${0}\n\tor\n\t${0} [USER]${NC}"
+        echo -e "${RED}Usage :\n\tsudo ${0}\n\tor\n\t${0} [USER]${NC}" | tee -a $LOGFILE
         exit
     fi
 
     if [ -n ${USERNAME} ]; then
         USERHOME=$(getent passwd ${USERNAME} | cut -d: -f6)
     else
-        echo -e "${RED}User not find${NC}"
+        echo -e "${RED}User not find${NC}" | tee -a $LOGFILE
         exit
     fi
 
     if [ -z ${USERHOME} ]; then
-        echo -e "${RED}${USERNAME} not exist on the system${NC}"
+        echo -e "${RED}${USERNAME} not exist on the system${NC}" | tee -a $LOGFILE
         exit
     fi
 else
-    echo -e "${RED}Please run as root${NC}"
+    echo -e "${RED}Please run as root${NC}" | tee -a $LOGFILE
     exit
 fi
 
-echo -e "${GREEN}INSTALLATION START !${NC}"
+echo -e "${GREEN}INSTALLATION START !${NC}" | tee -a $LOGFILE
 
-echo -e "${YELLOW}APT UPDATE${NC}"
+echo -e "${YELLOW}APT UPDATE${NC}" | tee -a $LOGFILE
 apt-get -y update >> $LOGFILE
 if [ $? -ne 0 ]; then
-    echo -e "${RED}UPDATE FAILED${NC}"
+    echo -e "${RED}UPDATE FAILED${NC}" | tee -a $LOGFILE
     exit
 fi
 
-echo -e "${YELLOW}APT INSTALL PACKAGES:${NC}"
+echo -e "${YELLOW}APT INSTALL PACKAGES:${NC}" | tee -a $LOGFILE
 for PACK in $PACKAGES; do
-    echo -e "${PURPLE}INSTALL ${PACK}${NC}"
+    echo -e "${PURPLE}INSTALL ${PACK}${NC}" | tee -a $LOGFILE
     apt-get -y install $PACK >> $LOGFILE
     if [ $? -ne 0 ]; then
-        echo -e "${RED}${PACK} INSTALLATION FAILED${NC}"
+        echo -e "${RED}${PACK} INSTALLATION FAILED${NC}" | tee -a $LOGFILE
         exit
     fi
 done
 
-echo -e "${YELLOW}COPY CONFIG FILES${NC}"
+echo -e "${YELLOW}COPY CONFIG FILES${NC}" | tee -a $LOGFILE
 cp $DIRPATH/srcs/tmux.conf $USERHOME/.tmux.conf
 cp $DIRPATH/srcs/vimrc $USERHOME/.vimrc
 
-echo -e "${YELLOW}INSTALL VIM PLUGIN MANAGER${NC}"
+echo -e "${YELLOW}INSTALL VIM PLUGIN MANAGER${NC}" | tee -a $LOGFILE
 su ${USERNAME} -c "curl -fLo $USERHOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" &>> $LOGFILE
 
-echo -e "${YELLOW}INSTALL VIM PLUGINS${NC}"
+echo -e "${YELLOW}INSTALL VIM PLUGINS${NC}" | tee -a $LOGFILE
 su ${USERNAME} -c "vim +'PlugInstall --sync' +quitall" &>> $LOGFILE
 
-echo -e "${YELLOW}INSTALL YOUCOMPLETEME PLUGIN${NC}"
+echo -e "${YELLOW}INSTALL YOUCOMPLETEME PLUGIN${NC}" | tee -a $LOGFILE
 su ${USERNAME} -c "python3 $USERHOME/.vim/plugged/YouCompleteMe/install.py" >> $LOGFILE
 
-echo -e "${GREEN}INSTALLATION SUCCESSFULL !${NC}"
+echo -e "${GREEN}INSTALLATION SUCCESSFULL !${NC}" | tee -a $LOGFILE
